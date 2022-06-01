57fd88936c670034b06b48a87fea57061e7c7b56
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-06-01 11:42:35 -0500

    [+] Routes: Define rutas de navegacion (expenses, invoices)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
 	return (
 		<div>
-			<h1>Bookkeeper</h1>
+			<h1>Bookkeeper!</h1>
 			<nav
 				style={{
 					borderBottom: "solid 1px",
--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Expenses from './routes/Expenses';
+import Invoices from './routes/Invoices';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Routes>
+				<Route path="/" element={ <App /> } />
+				<Route path="expenses" element={ <Expenses /> } />
+				<Route path="invoices" element={ <Invoices /> } />
+			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
 );
