75e7736052b4ea1a2c9a322163977031c6cb3f02
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-06-01 15:09:31 -0500

    [+] Invoices: Filtra usando parámetro de búsqueda (useSearchParams)

--- src/routes/Invoices.jsx
+++ src/routes/Invoices.jsx
@@ -1,10 +1,12 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
 
 import { getInvoices } from "../data";
 
 export default function Invoices() {
 
-    let invoices = getInvoices();
+    let
+        invoices = getInvoices(),
+        [ searchParams, setSearchParams ] = useSearchParams();  // * useSearchParams: Establecemos el valor de la entrada a lo que sea que esté en el parámetro de búsqueda de filtro (¡es como useState, pero en URLSearchParams en su lugar!)
 
     return (
         <main style={{ padding: "1rem 0" }}>
@@ -16,7 +18,31 @@ export default function Invoices() {
                         padding: "1rem",
                     }}
                 >
-                    {   invoices.map( ( invoice ) => (
+                    <input
+                        value={ searchParams.get( 'filter' ) || '' }
+                        onChange={ ( event ) => {
+                            let filter = event.target.value;
+                            
+                            
+                            if ( filter )
+                                setSearchParams({ filter });    // * setSearchParams() está poniendo los '?filter=...' parámetros de búsqueda en la URL y volviendo a renderizar el enrutador. 
+                            else 
+                                setSearchParams({});
+                            
+                        }}
+                    />
+
+                    {   invoices
+                            .filter( ( invoice ) => {           // * Filtramos nuestra lista de facturas según el parámetro de búsqueda de filtro.
+                                let filter = searchParams.get( 'filter' );
+                                
+                                if ( ! filter ) return true;
+
+                                let name = invoice.name.toLowerCase();
+
+                                return name.startsWith( filter.toLowerCase() );
+                            })
+                            .map( ( invoice ) => (
                             <NavLink
                                 className={ ({ isActive }) => isActive ? "link link-red" : "link link-blue" }
                                 to={ `/invoices/${ invoice.number }` }
